b7d3731beefc7dc02f80a30d54761591c2ab05f1
Damaris Castro <damaris.icastro@gmail.com>
2026-07-13 12:14:08 +0200

    feat(card): add Card container and fixed missing import in IconButton (#9)

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,1 +1,11 @@
-// Card
+import './Card.scss';
+
+const Card = ({ children, as: Tag = 'div', className = '', ...rest }) => {
+    return (
+        <Tag className={`card${className ? ` ${className}` : ''}`} {...rest}>
+            {children}
+        </Tag>
+    );
+};
+
+export default Card;
