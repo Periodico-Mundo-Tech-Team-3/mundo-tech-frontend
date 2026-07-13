ad37bde10356324da62c1f82237fb15011d5fe80
Damaris Castro <damaris.icastro@gmail.com>
2026-07-13 09:40:04 +0200

    feat(button): add Button component with primary, secondary, danger and danger-outline variants (#5)

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,1 +1,26 @@
-// Buttons
+import './Button.scss';
+
+const Button = ({
+    children,
+    variant = 'primary',
+    type = 'button',
+    icon: Icon,
+    disabled = false,
+    onClick,
+    ...rest
+}) => {
+    return (
+        <button
+            type={type}
+            className={`button button--${variant}`}
+            disabled={disabled}
+            onClick={onClick}
+            {...rest}
+            >
+            {Icon && <Icon className="button__icon" size={18} aria-hidden="true" />}
+            {children}
+        </button>
+    );
+};
+
+export default Button;
