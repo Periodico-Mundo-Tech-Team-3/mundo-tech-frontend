888195cac57d1808377288f82c42e21d2186317e
Damaris Castro <damaris.icastro@gmail.com>
2026-07-14 11:07:30 +0200

    feat(my-articles): assemble MyArticles page with permission-based filtering and tabs

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAuth } from '../context/AuthContext';
 import { MainLayout } from '../layouts/MainLayout';
+import MyArticles from "../pages/MyArticles/MyArticles.jsx";
 
 export const AppRouter = () => {
   const { user, debugSetUser } = useAuth();
