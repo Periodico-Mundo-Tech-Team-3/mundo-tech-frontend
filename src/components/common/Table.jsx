5d884e9cde6c56b4899afd195acc292e655fb619
Damaris Castro <damaris.icastro@gmail.com>
2026-07-14 09:28:16 +0200

    feat(table): add accessible Table with semantic markup and horizontal scroll (#16)

--- src/components/common/Table.jsx
+++ src/components/common/Table.jsx
@@ -1,1 +1,23 @@
-//Table
+import './Table.scss';
+
+const Table = ({ headers = [], children, caption }) => {
+     return (
+         <div className="table-wrapper">
+             <table className="table">
+                 {caption && <caption className="table__caption">{caption}</caption>}
+                 <thead className="table__head">
+                 <tr>
+                     {headers.map((header, index) => (
+                         <th key={index} scope="col" className="table__th">
+                             {header}
+                         </th>
+                     ))}
+                 </tr>
+                 </thead>
+                 <tbody className="table__body">{children}</tbody>
+             </table>
+         </div>
+     );
+};
+
+export default Table;
